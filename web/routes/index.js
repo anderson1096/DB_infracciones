df8d43148537b5c5138bcd419c7df05ace4a3ec5
Anderson Mosquera <anderson.mosquera9610@gmail.com>
2016-11-22 13:02:03 -0500

    Agregando la conexion con la base de datos y el uso de ejs para las views

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+
+/*------------ DATABASE CONNECTION ----------------*/
+var connection = mysql.createConnection({
+  host : 'localhost',
+  user : 'root',
+  password : 'ander1096',
+  database : 'C_infracciones'
+});
+
+connection.connect(function(err,res){
+  if(err){
+    throw err;
+  }else{
+    console.log('Conexion establecida');
+  }
+});
+
+/*-------------------------------------------------*/
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  connection.query('SELECT * FROM Multa',function(err,result){
+    if(err){
+      throw err;
+    }
+    res.render('index', { title: result[0].Articulo_Sancionado});
+
+  })
+
 });
 
 module.exports = router;
